4e68cb7404dfa98f1dfae48be23981dab28fe6fc
Kouhei Sutou <kou@clear-code.com>
2017-07-10 18:22:31 +0900

    Add tests for Web interface

---
+++ test/controllers/extractions_controller_test.rb
@@ -0,0 +1,164 @@
+require 'test_helper'
+
+class ExtractionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+  end
+
+  def run_http_server(content_type: nil, body: nil)
+    config = {
+      :Port => 0,
+      :Logger => Rails.logger,
+      :AccessLog => [
+        [Rails.logger, WEBrick::AccessLog::COMMON_LOG_FORMAT],
+      ],
+    }
+    server = WEBrick::HTTPServer.new(config)
+    server.mount_proc("/") do |request, response|
+      response.content_type = content_type
+      response.body = body
+    end
+    server_thread = Thread.new do
+      server.start
+    end
+    begin
+      port = server[:Port]
+      yield("http://127.0.0.1:#{port}/")
+    ensure
+      server.shutdown
+      server_thread.join
+    end
+  end
+
+  sub_test_case "browser" do
+    test "root" do
+      get root_url
+      assert_redirected_to extraction_url
+    end
+
+    test "#show" do
+      get extraction_url
+      assert_response :success
+    end
+
+    sub_test_case "#create" do
+      test "no params" do
+        visit(extraction_url)
+        click_button("Extract")
+        within("#error_explanation") do
+          assert_equal(["Data can't be blank"],
+                       all("li").collect(&:text))
+        end
+      end
+
+      test "URI" do
+        visit(extraction_url)
+        run_http_server(content_type: "text/plain", body: "Hello") do |uri|
+          fill_in("URI", with: uri.to_s)
+          click_button("Extract")
+          assert_equal(["Hello"],
+                       all(".text pre").collect(&:text))
+        end
+      end
+
+      sub_test_case "data" do
+        def assert_extract(expected, fixture_name)
+          visit(extraction_url)
+          attach_file("Data", file_fixture(fixture_name))
+          click_button("Extract")
+          extracted = all(".text").collect do |node|
+            [
+              node.find(".metadata-title td").text,
+              node.find(".body").text,
+            ]
+          end
+          assert_equal(expected, extracted)
+        end
+
+        test "HTML" do
+          assert_extract([["Hello", "World!"]], "hello.html")
+        end
+
+        test "OpenDocument Text" do
+          assert_extract([["Hello", "World!"]], "hello.odt")
+        end
+
+        test "Word: old" do
+          assert_extract([["Hello", "World!"]], "hello.doc")
+        end
+
+        test "Word" do
+          assert_extract([["Hello", "World!"]], "hello.docx")
+        end
+
+        test "OpenDocument Spreadsheet" do
+          assert_extract([
+                           [
+                             "Hello",
+                             "Sheet1 A1 Sheet1 A2 Sheet1 B1 " +
+                             "Sheet2 A1 Sheet2 A2 Sheet2 B1",
+                           ],
+                         ],
+                         "hello.ods")
+        end
+
+        test "Excel: old" do
+          assert_extract([
+                           [
+                             "Hello",
+                             "Sheet1 A1 Sheet1 A2 Sheet1 B1 " +
+                             "Sheet2 A1 Sheet2 A2 Sheet2 B1",
+                           ],
+                         ],
+                         "hello.xls")
+        end
+
+        test "Excel" do
+          assert_extract([
+                           [
+                             "Hello",
+                             "Sheet1 A1 Sheet1 A2 Sheet1 B1 " +
+                             "Sheet2 A1 Sheet2 A2 Sheet2 B1",
+                           ],
+                         ],
+                         "hello.xlsx")
+        end
+
+        test "OpenDocument Presentation" do
+          assert_extract([
+                           [
+                             "Hello",
+                             "Page1 Title Page1 Content " +
+                             "Page2 Title Page2 Content",
+                           ]
+                         ],
+                         "hello.odp")
+        end
+
+        test "PowerPoint: old" do
+          assert_extract([
+                           [
+                             "Hello",
+                             "Page1 Title Page1 Content " +
+                             "Page2 Title Page2 Content",
+                           ]
+                         ],
+                         "hello.ppt")
+        end
+
+        test "PowerPoint" do
+          assert_extract([
+                           [
+                             "Hello",
+                             "Page1 Title Page1 Content " +
+                             "Page2 Title Page2 Content",
+                           ]
+                         ],
+                         "hello.pptx")
+        end
+      end
+    end
+  end
+
+  sub_test_case "API" do
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'test/unit/rails/test_help'
+require 'webrick'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
